22f8734d395644865f8996fb7acbec8a0e72e0d7
Nadezhda Kirilenko <Nadezhda.Kirilenko@lanit-tercom.com>
2024-11-20 20:52:51 +0300

    решение задач 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+  btn.addEventListener('click', () => {
+    if (!text.hidden) {
+      text.hidden = true
+    } else {
+      text.hidden = false
+    };
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let size = 0;
+  let btnRight = document.querySelector('.carousel__arrow_right');
+  let btnLeft = document.querySelector('.carousel__arrow_left');
+  let carousel = document.querySelector('.carousel__inner');
+  let width = carousel.offsetWidth;
+  btnLeft.style.display = 'none';
+
+  btnRight.addEventListener('click', () => {
+    size = size - width;
+    btnLeft.style.display = '';
+    carousel.style.transform = `translateX(${size}px)`; 
+    if (size <= -3 * width) {
+      btnRight.style.display = 'none'
+    }else{ 
+      btnRight.style.display = ''
+    }
+      ;
+  });
+  btnLeft.addEventListener('click', () => {
+    size = size + width;
+    btnRight.style.display = ''
+    carousel.style.transform = `translateX(${size}px)`;
+    if (size >= 0) {
+      btnLeft.style.display = 'none'
+    } else {
+      btnLeft.style.display = ''
+    };
+     });  
 }
