8d8d040f2cd63ae879f128c665f4c5e791b1d817
Nadezhda Kirilenko <Nadezhda.Kirilenko@lanit-tercom.com>
2024-12-03 18:16:28 +0300

    решение 1 и 2 задачи 6 модуля

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,49 @@
  *
  */
 export default class UserTable {
+  rows;
+  elem;
+ 
   constructor(rows) {
+    this.rows = rows;
+    this.#render();
+    this.#deleteRow();
+  }
+  
+  #template() {
+    return `<table> 
+        <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+        </thead>
+          <tbody>
+           ${this.rows.map(row =>
+          `<tr>
+          <td>${row.name}</td>
+          <td>${row.age}</td>
+          <td>${row.salary}</td>
+          <td>${row.city}</td>
+          <td><button>X</button></td>
+        </tr>`).join('')};
+      </tbody>
+      </table>
+      `
+  }
+  
+   #render() {
+   this.elem = document.createElement('table');
+   this.elem.innerHTML = this.#template();  
+  }
+  
+   #deleteRow() {
+    for (const button of this.elem.querySelectorAll("button"))
+      button.addEventListener('click', (event) =>
+      event.target.closest("tr").remove())
   }
 }
+
